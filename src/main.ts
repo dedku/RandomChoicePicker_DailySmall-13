ab09113f5d5062926d59db9266ae3efbe93beae7
Patryk Szabat <szabat.patryk@gmail.com>
2023-01-05 23:25:42 +0100

    css + ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,71 @@
 import './style.css'
+
+const tagsEl = document.getElementById('tags') as HTMLDivElement
+const textarea = document.getElementById('textarea') as HTMLTextAreaElement
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e: KeyboardEvent | null) => {
+    createTags(textarea.value)
+
+    if (e!.key === 'Enter') {
+        setTimeout(() => {
+            textarea.value = ''
+        }, 10)
+
+        randomSelect()
+    }
+})
+
+function createTags(input: string) {
+    const tags = input!.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    })
+}
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        if (randomTag !== undefined) {
+            highlightTag(randomTag)
+
+            setTimeout(() => {
+                unHighlightTag(randomTag)
+            }, 100)
+        }
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+
+    }, times * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag: Element) {
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag: Element) {
+    tag.classList.remove('highlight')
+}
